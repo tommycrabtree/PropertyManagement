8eda6d9f3c0459614f5974cf8968ab429e9ebc92
Tommy <tommycrabtree@outlook.com>
2023-01-01 22:40:35 -0600

    PropertyType table

---
+++ PropertyDB/dbo/Tables/PropertyType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PropertyType]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [TypeOfProperty] NVARCHAR(40) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL, 
+    [LastUpdate] DATETIME2 NOT NULL
+)
