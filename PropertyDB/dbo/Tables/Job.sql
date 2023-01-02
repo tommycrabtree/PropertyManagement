88e6a3ea88860e9c1bd4ac72eb553905617ac8bc
Tommy <tommycrabtree@outlook.com>
2023-01-01 23:05:28 -0600

    Job Table creation

---
+++ PropertyDB/dbo/Tables/Job.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Job]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [WorkingEmployeeName] NVARCHAR(40) NOT NULL, 
+    [WorkingEmployeeCompanyName] NVARCHAR(60) NOT NULL, 
+    [WorkAssignedDate] DATETIME2 NOT NULL, 
+    [WorkAcceptanceDate] DATETIME2 NULL, 
+    [WorkDelegatedDate] DATETIME2 NULL, 
+    [WorkRefusedDate] DATETIME2 NULL, 
+    [WorkAssignedBy] NVARCHAR(40) NOT NULL, 
+    [WorkAssignedByCompanyName] NVARCHAR(40) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL, 
+    [LastUpdate] DATETIME2 NOT NULL
+)
--- PropertyDB/dbo/Tables/PropertyType.sql
+++ PropertyDB/dbo/Tables/PropertyType.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [TypeOfProperty] NVARCHAR(40) NOT NULL, 
     [CreateDate] DATETIME2 NOT NULL, 
-    [LastUpdate] DATETIME2 NOT NULL
+    [LastUpdate] DATETIME2 NOT NULL, 
+    [ZoneStatus] NVARCHAR(40) NOT NULL
 )
